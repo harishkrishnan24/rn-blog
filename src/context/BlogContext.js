b274e41186aa5599380737d7da28228764c8b2ff
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-27 11:03:58 +0530

    Add edit feature

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,6 +13,13 @@ const blogReducer = (state, action) => {
 			];
 		case "delete_blogpost":
 			return state.filter((blogPost) => blogPost.id !== action.payload);
+		case "edit_blogpost":
+			return state.map((blogPost) => {
+				if (blogPost.id === action.payload.id) {
+					return action.payload;
+				}
+				return blogPost;
+			});
 		default:
 			return state;
 	}
@@ -21,7 +28,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
 	return (title, content, callback) => {
 		dispatch({ type: "add_blogpost", payload: { title, content } });
-		callback();
+		if (callback) {
+			callback();
+		}
 	};
 };
 
@@ -31,8 +40,23 @@ const deleteBlogPost = (dispatch) => {
 	};
 };
 
+const editBlogPost = (dispatch) => {
+	return (id, title, content, callback) => {
+		dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+		if (callback) {
+			callback();
+		}
+	};
+};
+
 export const { Context, Provider } = createDataContext(
 	blogReducer,
-	{ addBlogPost, deleteBlogPost },
-	[]
+	{ addBlogPost, deleteBlogPost, editBlogPost },
+	[
+		{
+			title: "React Native Course",
+			content: "Best Course in Udemy platform",
+			id: 1,
+		},
+	]
 );
